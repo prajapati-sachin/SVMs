bb70dcaa92185682a4e37f7f9200dd5bf3d0d954
prajapati-sachin <sachinkp1211@gmail.com>
2019-03-14 17:22:54 +0530

    Multi using LIBSVM

---
+++ libsvmmulti.py
@@ -0,0 +1,80 @@
+import numpy as np
+from cvxopt import matrix
+from cvxopt import solvers
+import csv
+import time
+from numpy import linalg as la
+import math
+from svmutil import *
+import sys
+import random
+# X = []
+# Y = []
+# X_test = []
+# Y_test = []
+
+# train = sys.argv[1]
+# test = sys.argv[2]
+# part = sys.argv[3]
+
+# time_gap = float(sys.argv[4])
+
+
+
+X = []
+Y = []
+X_test = []
+Y_test = []
+
+# num = 5
+
+start1 = time.time()
+
+train = "mnist/train.csv"
+test = "mnist/test.csv"
+
+with open(train) as fileX:
+	x_reader = csv.reader(fileX)
+	for row in x_reader:
+		temp = []
+		for i in range(784):
+			temp.append(float(row[i])/255)
+		Y.append(float(row[784]))
+		X.append(temp)
+
+
+with open(test) as fileX:
+	x_reader = csv.reader(fileX)
+	for row in x_reader:
+		temp = []
+		for i in range(784):
+			temp.append(float(row[i])/255)
+		Y_test.append(float(row[784]))
+		X_test.append(temp)
+
+end1 = time.time()
+print("Input done, Time taken(sec)", int(end1-start1))
+start2 = time.time()
+# alpha_count = len(Xq1)
+
+prob  = svm_problem(Y, X)
+param = svm_parameter('-t 2 -c 1 -b 0 -g 0.05 -q')
+m = svm_train(prob, param, '-q')
+p_label, p_acc, p_val = svm_predict(Y_test, X_test, m, '-b 0 -q')
+# print("Accuracy using LIBSVM: ", p_acc)
+ACC, MSE, SCC = evaluations(Y_test, p_label)
+print("Accuracy using LIBSVM: ", ACC)
+
+end2 = time.time()
+print("Time taken(sec)", int(end2-start2))
+
+	# alpha_libsvm = m.get_sv_coef()
+	# SV_indices = m.get_sv_indices()
+	# alpha_svm = []
+	# j=0
+	# for i in range(len(Yq1_test)):
+	# 	if(i==SV_indices[j]):
+	# 		alpha_svm.append(alpha_libsvm[j][0])
+	# 		j+=1
+	# 	else:
+	# 		alpha_svm.append(0)
---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+a=1
+b=2
+# c=3
+# d=4
+aa=a
+bb=b
+cc=c
+
+
+if [ $1 == $a ]; then 
+    python3 q1.py $2 $3 $4 $5 
+elif [ $1 == $b ]; then 
+    if [ $4 == $aa ]; then 
+        python3 svm.py $2 $3 $4 
+    elif [ $4 == $bb ]; then 
+        python3 svm.py $2 $3 $4 
+    elif [ $4 == $cc ]; then 
+        python3 libsvm.py $2 $3 $4     
+    fi
+    # python3 q2.py $2 $3 $4 
+fi
+
+
